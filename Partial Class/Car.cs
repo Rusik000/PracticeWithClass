333817076564cce7d5014cd7d0ea1201ddfff6ae
Must_nu73@itstep.edu.az <Must_nu73@itstep.edu.az>
2021-06-26 15:14:58 +0400

    Add project files.

---
+++ Partial Class/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+
+    public class Car
+    {
+
+        public int Id { get; set; }
+        public string Model { get; set; } = String.Empty;
+        public string Vendor { get; set; } = String.Empty;
+        public double Volume { get; set; }
+        public DateTime ProduceYear { get; set; } = DateTime.Now;
+        public Car(in int id, in string model, in string vendor, in double volume, in DateTime produceYear)
+        {
+            Id = id;
+            Model = model;
+            Vendor = vendor;
+            Volume = volume;
+            ProduceYear = produceYear;
+        }
+        public double Age
+        {
+            get
+            {
+                var years = DateTime.Now.Year - ProduceYear.Year;
+                return years;
+            }
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Car ID: {Id}");
+            Console.WriteLine($"Car Model: {Model}");
+            Console.WriteLine($"Car Vendor: {Vendor}");
+            Console.WriteLine($"Car Volume: {Volume}");
+            Console.WriteLine($"Car Produce Year: {ProduceYear.ToString("F")}");
+            Console.WriteLine($"Car Age: {Age}");
+        }
+    }
+}
---
+++ Partial Class/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Classes
+{
+    class Program
+    {
+        public static void Run()
+        {
+            //Tapsiriq 1
+
+            //var point = new Point(256, 456);
+            //point.Show();
+
+
+
+            ////Tapsiriq 2 
+
+            //var counterSecond = new Counter(0, 100);
+
+            //for (int i = 0; i < 150; i++)
+            //{
+            //    if (i == 100)
+            //    {
+
+            //        Console.ForegroundColor = ConsoleColor.Green;
+            //        Console.WriteLine();
+            //    }
+            //    Thread.Sleep(500);
+            //    Console.WriteLine();
+            //    Console.Write(counterSecond.Increment() + " ");
+            //}
+
+            //Console.WriteLine();
+
+
+            ////Tapsiriq 3
+
+            //var first = new Fraction(16, 1);
+            //var second = new Fraction(5, 4);
+
+
+            //Console.ForegroundColor = ConsoleColor.Cyan;
+            //Console.Write("First Fraction :");
+            //first.Show();
+            //Console.ForegroundColor = ConsoleColor.Yellow;
+            //Console.Write("Second Fraction :");
+            //second.Show();
+            //Console.ResetColor();
+
+
+            //Console.ForegroundColor = ConsoleColor.Red;
+            //Console.WriteLine();
+            //Console.WriteLine(first / second);
+            //Console.WriteLine(first * second);
+            //Console.WriteLine(first + second);
+            //Console.WriteLine(first - second);
+            //Console.WriteLine();
+            //Console.ResetColor();
+
+
+            //Tapsiriq 4
+            Console.ForegroundColor = ConsoleColor.Green;
+            var car = new Car(1, "Bmw", "525", 2.5, new DateTime(2002, 10, 8));
+            car.Show();
+        }
+
+        static void Main(string[] args)
+        {
+
+            Run();
+        }
+
+    }
+}
+
